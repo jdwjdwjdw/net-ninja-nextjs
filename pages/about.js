4f7919a14c4e852e4fb04dc8fea45a825c5f59fd
jdwjdwjdw <jacobdeanwilson@gmail.com>
2021-02-06 01:35:01 -0800

    Add initial pages and routes

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam assumenda, est atque quo praesentium distinctio cumque aut, libero dolore neque eveniet expedita sit iste quis enim asperiores quas quod. Aut?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam assumenda, est atque quo praesentium distinctio cumque aut, libero dolore neque eveniet expedita sit iste quis enim asperiores quas quod. Aut?</p>
+        </div>
+     );
+}
+ 
+export default About;
