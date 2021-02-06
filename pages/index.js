5bd465f5e23a364a5b2862288697fea396698a10
jdwjdwjdw <jacobdeanwilson@gmail.com>
2021-02-06 13:41:45 -0800

    Add links between pages using next Link component

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -10,6 +11,9 @@ export default function Home() {
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis beatae quos modi nisi. Debitis, placeat. Pariatur quos sint consequuntur et temporibus. Quia et corrupti voluptas quod aperiam maiores doloribus eius.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis beatae quos modi nisi. Debitis, placeat. Pariatur quos sint consequuntur et temporibus. Quia et corrupti voluptas quod aperiam maiores doloribus eius.</p>
+      <Link href="/ninjas">
+        <a>See Ninja Listing</a>
+      </Link>
       <Footer />
     </div>
   )
