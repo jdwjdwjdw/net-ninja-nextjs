62e3998438196448c986b5335665460de60f464c
jdwjdwjdw <jacobdeanwilson@gmail.com>
2021-02-10 17:43:03 -0800

    Add metadata using next Head component

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,18 @@
+import Head from 'next/head'
+
 const About = () => {
-    return ( 
-        <div>
-            <h1>About</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam assumenda, est atque quo praesentium distinctio cumque aut, libero dolore neque eveniet expedita sit iste quis enim asperiores quas quod. Aut?</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam assumenda, est atque quo praesentium distinctio cumque aut, libero dolore neque eveniet expedita sit iste quis enim asperiores quas quod. Aut?</p>
-        </div>
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas"/>
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam assumenda, est atque quo praesentium distinctio cumque aut, libero dolore neque eveniet expedita sit iste quis enim asperiores quas quod. Aut?</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam assumenda, est atque quo praesentium distinctio cumque aut, libero dolore neque eveniet expedita sit iste quis enim asperiores quas quod. Aut?</p>
+            </div>
+        </>
      );
 }
  
